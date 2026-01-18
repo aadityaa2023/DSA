ff3bd7f182f6251d1c0290dfdd8190395adebcdb
aadityaa2023 <adityamishraniet2023@gmail.com>
2026-01-18 14:01:06 +0530

    Fix prime number logic and improve output messages

--- programming/prime.java
+++ programming/prime.java
@@ -1,29 +1,29 @@
 import java.util.*;
+
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter the val of num: ");
+
+        System.out.print("Enter the value of num: ");
         int num = sc.nextInt();
+
         boolean isPrime = true;
-        
-        if (num<=1){
-            isPrime = true;
-        }else{
-            for(int i =2; i<num/2; i++){
-                if (num % i ==0){
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
                     isPrime = false;
                     break;
                 }
             }
         }
-        if(isPrime){
-            System.out.println("Prime");
-            
-        }
-        else{
-            System.out.println("NOt Prime");
-        }
 
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
     }
 }
